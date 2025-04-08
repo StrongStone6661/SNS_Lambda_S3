e0671cd7a2d881f287c677d68d8c084fe9dc5ffe
AsanteGyabengSamuel <samuel.asante@amalitech.com>
2025-04-08 12:51:02 +0000

    refactor

--- src/main/java/org/sendemail/Main.java
+++ src/main/java/org/sendemail/Main.java
@@ -1,9 +1,49 @@
 package org.sendemail;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.services.lambda.runtime.events.S3Event;
+import software.amazon.awssdk.services.sns.SnsClient;
+import software.amazon.awssdk.services.sns.model.PublishRequest;
+import software.amazon.awssdk.regions.Region;
+
+public class Main implements RequestHandler<S3Event, String> {
+
+    // Environment variable for SNS topic ARN
+    private static final String SNS_TOPIC_ARN = System.getenv("SNS_TOPIC_ARN");
+
+    @Override
+    public String handleRequest(S3Event s3Event, Context context) {
+        // Create SNS client
+        SnsClient snsClient = SnsClient.builder()
+                .region(Region.EU_CENTRAL_1) // Replace with your region
+                .build();
+
+
+        try {
+            // Get the S3 bucket and object key from the event
+            String bucketName = s3Event.getRecords().get(0).getS3().getBucket().getName();
+            String objectKey = s3Event.getRecords().get(0).getS3().getObject().getKey();
+
+            // Create the message
+            String message = String.format("New file uploaded to S3!\nBucket: %s\nFile: %s", bucketName, objectKey);
+
+            // Publish to SNS topic
+            PublishRequest publishRequest = PublishRequest.builder()
+                    .topicArn(SNS_TOPIC_ARN)
+                    .message(message)
+                    .subject("S3 Upload Notification")
+                    .build();
+
+            snsClient.publish(publishRequest);
+            context.getLogger().log("Published SNS message: " + message);
+
+            return "Success";
+        } catch (Exception e) {
+            context.getLogger().log("Error: " + e.getMessage());
+            throw new RuntimeException(e);
+        } finally {
+            snsClient.close();
+        }
     }
 }
